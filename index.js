af64482e063b414f49c0b7cac9ee7aab774e1a2b
Florian BARBARE <florian.barbare@gmail.com>
2018-02-22 18:13:54 +0000

    fix name element

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export Box from './lib/Box';
 export Button from './lib/Button';
-export Checkout from './lib/Checkout';
+export Checkbox from './lib/Checkbox';
 export Icon from './lib/Icon';
 export Loader from './lib/Loader';
 export Radio from './lib/Radio';
