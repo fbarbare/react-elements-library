054e2bbd825e8e170ac3f6704ca949d98c275c69
Florian BARBARE <florian.barbare@gmail.com>
2018-02-22 17:34:27 +0000

    babel build

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Text = exports.Select = exports.Radio = exports.Loader = exports.Icon = exports.Checkout = exports.Button = exports.Box = undefined;
+
+var _Box2 = require('./lib/Box');
+
+var _Box3 = _interopRequireDefault(_Box2);
+
+var _Button2 = require('./lib/Button');
+
+var _Button3 = _interopRequireDefault(_Button2);
+
+var _Checkout2 = require('./lib/Checkout');
+
+var _Checkout3 = _interopRequireDefault(_Checkout2);
+
+var _Icon2 = require('./lib/Icon');
+
+var _Icon3 = _interopRequireDefault(_Icon2);
+
+var _Loader2 = require('./lib/Loader');
+
+var _Loader3 = _interopRequireDefault(_Loader2);
+
+var _Radio2 = require('./lib/Radio');
+
+var _Radio3 = _interopRequireDefault(_Radio2);
+
+var _Select2 = require('./lib/Select');
+
+var _Select3 = _interopRequireDefault(_Select2);
+
+var _Text2 = require('./lib/Text');
+
+var _Text3 = _interopRequireDefault(_Text2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+exports.Box = _Box3['default'];
+exports.Button = _Button3['default'];
+exports.Checkout = _Checkout3['default'];
+exports.Icon = _Icon3['default'];
+exports.Loader = _Loader3['default'];
+exports.Radio = _Radio3['default'];
+exports.Select = _Select3['default'];
+exports.Text = _Text3['default'];
